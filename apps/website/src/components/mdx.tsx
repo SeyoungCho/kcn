c2d96448de1ac4fa91e59254c207e732d84e82f0
SeyoungCho <wnl383@naver.com>
2026-04-23 19:00:29 +0900

    refactor: use @repo/.. for package resolution

--- apps/website/src/components/mdx.tsx
+++ apps/website/src/components/mdx.tsx
@@ -1,9 +1,11 @@
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
+import TFlavoredButton from "@repo/t-flavored/ui/button";
 
 export function getMDXComponents(components?: MDXComponents) {
   return {
     ...defaultMdxComponents,
+    TFlavoredButton,
     ...components,
   } satisfies MDXComponents;
 }
